f459d34ae3f98bc2736453bcb70e51fd6fbe64de
andrew lindsay <andrewlindsay97@gmail.com>
2018-05-07 19:28:50 +0100

    removed useless code and created markdown file

---
+++ README.md
@@ -0,0 +1,6 @@
+#Misc Scripts
+Miscellaneous scripts 
+##Tv show renaming script
+Python scripts for mass renaming of tv show episodes 
+##Remove unwanted windows apps (powershell)
+Amos' scripted that killed his PC 
--- Tv_show_rename_script/dir_re.py
+++ Tv_show_rename_script/dir_re.py
@@ -1,26 +1,15 @@
 import os
 import sys
-import time
+
 # variables
-file_list  = []
+file_list = []
 count = 0
 cwd = os.getcwd()
 file_ending = ""
 left_offset = 0
 right_offset = 0
 
-def rm_underscore(str1):
-    str2 = str1[0:len(str1) -12]
-    str_out = ""
-    for ch in str2:
-        if ch == '_':
-            str_out = str_out + " "
-        else:
-            str_out = str_out + ch
-    str_out = str_out + str1[len(str1) -12 : len(str1)]
-    return str_out
-
-# script starts here 
+# script starts here
 print("Welcome to the renamer")
 print("Current working dir: " + cwd)
 
@@ -40,7 +29,7 @@ for _file in file_list:
 
 while True:
     yes_1 = ""
-    yes_2 = "" 
+    yes_2 = ""
     left_offset = raw_input("Enter number of characters from left to remove:")
     right_offset = raw_input("Enter number of characters from right to remove:")
     season = raw_input("Enter season number:")
@@ -49,13 +38,12 @@ while True:
 
     for t in file_list:
         name = file_list[count]
-        if (count + 1 > 9 ) :
-            name = str(count + 1) + " - " + name[int(left_offset):(len(name)-int(right_offset))] + "_s0" + season + "e" + str(count + 1) + "." + file_ending
-            
+        if (count + 1 > 9 ):
+            name = str(count + 1) + " - " + name[int(left_offset):(len(name) - int(right_offset))] + "_s0" + season + "e" + str(count + 1) + "." + file_ending
         else:
-            name = str(count + 1) + " - " + name[int(left_offset):(len(name)-int(right_offset))] + "_s0" + season + "e0" + str(count + 1) + "." + file_ending
-        print( t + " <-> " + name)
-        names.append(name)  
+            name = str(count + 1) + " - " + name[int(left_offset):(len(name) - int(right_offset))] + "_s0" + season + "e0" + str(count + 1) + "." + file_ending
+        print(t + " <-> " + name)
+        names.append(name)
         print("")
         count += 1
 
@@ -64,11 +52,11 @@ while True:
 
     if yes_1 == "y":
         yes_2 = raw_input("Are you sure?,\nEnter y/n: ")
-
+        
         if yes_2 == "y":
-            for x in range(0,len(names)):
-                print( file_list[x] + " <-> " + names[x])
-                os.rename(cwd + "\\"+ file_list[x], cwd + "\\"+ names[x])
+            for x in range(0, len(names)):
+                print(file_list[x] + " <-> " + names[x])
+                os.rename(cwd + "\\" + file_list[x], cwd + "\\" + names[x])
             sys.exit(0)
         else:
             continue
@@ -77,17 +65,4 @@ while True:
         if _exit == "exit":
             sys.exit(0)
         else:
-            continue
-            
-   
-            
-
-        
-#for t in list1:
-#    name = list1[count]
-#    name = name[0:len(name)-7] + name[len(name)-6:len(name)]
-#    print( t + " <-> " + name)
-#    print()
-##    os.rename(cwd + "\\"+ t,cwd + "\\"+ name  ) 
-#    count += 1 
-    
+            continue   
