1150b558adaf0f83ebade8a086c50e26936daa97
andrew lindsay <andrewlindsay97@gmail.com>
2018-05-12 18:08:39 +0100

    completed argument passing, fix exiting with ctrl-c, changed the amount of information printed to screen

--- Tv_show_rename_script/dir_re.py
+++ Tv_show_rename_script/dir_re.py
@@ -2,15 +2,36 @@ import os
 import sys
 import argparse
 
+#  surprise Totoro
+#    _____
+#   /     \\
+#   vvvvvvv  /|__/|
+#      I   /O,O   |
+#      I /_____   |      /|/|
+#     J|/^ ^ ^ \  |    /00  |    _//|
+#      |^ ^ ^ ^ |W|   |/^^\ |   /oo |
+#       \m___m__|_|    \m_m_|   \mm_|
 
-def take_command_line():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--season', '-s', action='store', help='Enter season of show', type=int)
-    parser.add_argument('--left', '-l', action='store', help='Amount to cut from left', type=int)
-    parser.add_argument('--right', '-r', action='store', help='Amount to cut from right', type=int)
-    parser.add_argument('--ending', '-e', action='store', help='type of files to alter e.g mkv', type=int)
+
+def parse_command_line():
+    parser = argparse.ArgumentParser(prog='Re_namer', formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=
+'''  _______    __   ____                                            
+ /_  __/ |  / /  / __ \___        ____  ____ _____ ___  ___  _____
+  / /  | | / /  / /_/ / _ \______/ __ \/ __ `/ __ `__ \/ _ \/ ___/
+ / /   | |/ /  / _, _/  __/_____/ / / / /_/ / / / / / /  __/ /    
+/_/    |___/  /_/ |_|\___/     /_/ /_/\__,_/_/ /_/ /_/\___/_/
+
+Description:    
+    This script is to be utilised to rename a season of a tv series so that it 
+    can be detected and properly parsed by the kodi media manager.''')
+    parser.add_argument('--season', '-s', action='store', help='Season of show', type=int, default=0)
+    parser.add_argument('--left', '-l', action='store', help='Cut from left', type=int, default=0)
+    parser.add_argument('--right', '-r', action='store', help='Cut from right', type=int, default=0)
+    parser.add_argument('--ending', '-e', action='store', help='Type of files to select (e.g mkv, mp4)', type=str, required=True)
     args_parsed = parser.parse_args()
-    return args_parsed.season, args_parsed.ending, args_parsed.left, args_parsed.right
+    return str(args_parsed.season), args_parsed.ending, args_parsed.left, args_parsed.right
+
 
 def change_file_names(file_list, season, left_offset, right_offset, file_ending):
     """alters file names and prints old and new to screen"""
@@ -36,72 +57,92 @@ def commit_name_change(file_list, new_names, cwd):
         os.rename(cwd + "\\" + file_list[x], cwd + "\\" + new_names[x])
 
 
-def main():
-    # variables
-    file_list = []
-    cwd = os.getcwd()
-    file_ending = ""
-    left_offset = 0
-    right_offset = 0
-
-    # script starts here
-    print("Welcome to the re-namer")
-    print("Current working dir: " + cwd)
-
-    file_ending = raw_input("Please enter the type of file to use (e.g mkv, mp4): ")
-
-    # gets only those that have ending of file
-    for _file in os.listdir(cwd):
-        if _file.endswith(file_ending):
-            file_list.append(_file)
-
-    # sort into order
-    file_list.sort()
-
-    # prints list of file that will be renamed
-    print("The files you have selected are:")
-    for _file in file_list:
-        print(_file)
-
-    # TODO: fix
-    # if not len(sys.argv) is 0:
-    #     for arg in sys.argv:
-    #         pass
-
+def yes_or_no(message='Enter y/n: '):
     while True:
-        yes_1 = ""
-        yes_2 = ""
+        y_or_n = raw_input(message)
+        if y_or_n is 'y':
+            dire = True
+            break
+        elif y_or_n is 'n':
+            dire = False
+            break
+        else:
+            print('not a valid option')
+    return dire
+
+
+def main():
+    try:
+        # variables
+        file_list = []
+        cwd = os.getcwd()
+        file_ending = ''
+        left_offset = 0
+        right_offset = 0
+        season = 0
+
+        args_passed = len(sys.argv) > 1
+
+        print("Welcome to TV Re-namer")
+        # TODO: be able to set current working directory, maybe by command line
+        print("Current working dir: " + cwd)
+
+        # if args npt passed get user input
+        if not args_passed:
+            file_ending = raw_input("Please enter the type of file to use (e.g mkv, mp4): ")
+        else:
+            season, file_ending, left_offset, right_offset = parse_command_line()
+
+        # gets only those that have ending matching file_ending
+        for _file in os.listdir(cwd):
+            if _file.endswith(file_ending):
+                file_list.append(_file)
+
+        file_list.sort()
+
+        # prints list of file that will be renamed
+        if not args_passed:
+            print("The files you have selected are:")
+            for _file in file_list:
+                print(_file)
+
+        # get season from user if not passed
+        if season is 0:
+            while True and not args_passed:
+                try:
+                    season = str(int(raw_input("Enter season number: ")))
+                    break
+                except ValueError:
+                    print("Please enter an integer")
 
         while True:
-            try:
-                left_offset = int(raw_input("Enter number of characters from left to remove:"))
-                right_offset = int(raw_input("Enter number of characters from right to remove:"))
-                season = str(int(raw_input("Enter season number:")))
-                break
-            except ValueError:
-                print("please enter a numerical numbers only")
 
-        # returns list of names after they have been renamed
-        new_names = change_file_names(file_list, season, left_offset, right_offset, file_ending)
+            while True and not args_passed:
+                try:
+                    left_offset = int(raw_input("Enter number of characters from left to remove: "))
+                    right_offset = int(raw_input("Enter number of characters from right to remove: "))
+                    break
+                except ValueError:
+                    print("please enter a numerical numbers only")
 
-        print("Do you want to replace names")
-        yes_1 = raw_input("Enter y/n: ")
+            # returns list of names after they have been renamed
+            new_names = change_file_names(file_list, season, left_offset, right_offset, file_ending)
 
-        if yes_1 == "y":
-            yes_2 = raw_input("Are you sure?,\nEnter y/n: ")
-            if yes_2 == "y":
-                commit_name_change(file_list, new_names, cwd)
-                raw_input("Re-naming complete press any key to continue... ")
-                sys.exit(0)
-            else:
-                continue
-        else:
-            _exit = raw_input("exit or continue: ")
-            if _exit == "exit":
-                sys.exit(0)
-            else:
-                continue
+            print("Do you want to replace names")
+            if yes_or_no():
+                if yes_or_no("Are you sure?\nEnter y/n: "):
+                    commit_name_change(file_list, new_names, cwd)
+                    raw_input("Re-naming complete press any key to continue... ")
+                    sys.exit(0)
+
+            # else:
+            #     if yes_or_no("Exit? y/n: "):
+            #         sys.exit(0)
+
+            args_passed = False
+    except:
+        pass
 
 
 if __name__ == "__main__":
-    print(take_command_line())
+    main()
